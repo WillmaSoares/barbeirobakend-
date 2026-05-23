da2b3a269fee25329dd89b7b32e51e1f1cebc916
Willma Soares <willmasilva.ws@gmail.com>
2026-05-23 16:03:50 -0300

    feat: adiciona JWT, login, logout e filtro de autenticacao

--- src/main/java/com/barbearia/backend/repository/UserRepository.java
+++ src/main/java/com/barbearia/backend/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.barbearia.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.barbearia.backend.enums.Role;
 import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRole(Role role);
+    Optional<User> findByEmail(String email);
+
 }
--- src/main/java/com/barbearia/backend/security/SecurityConfig.java
+++ src/main/java/com/barbearia/backend/security/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.barbearia.backend.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,15 +15,20 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/auth/login", "/auth/logout").permitAll()
+                        .requestMatchers("/users").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
